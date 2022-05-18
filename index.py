63a95af6d374b654342c169f8bc600a751ece3b4
soujatyabhat <soujatya.bhat2000@gmail.com>
2022-05-18 19:42:56 +0530

    First Commit

---
+++ index.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 19:33:15 2022
+
+@author: souja
+"""
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome"
+    
+@app.hello("/how are you")
+def hello():
+    return "I am Good. What about u"
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",post=8080)
+    

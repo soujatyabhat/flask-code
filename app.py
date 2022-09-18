7d6e30ac1b06c57c2245a27064170c5aaf5f82b8
Soujatya Bhattacharya <65819358+soujatyabhat@users.noreply.github.com>
2022-09-18 13:37:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 19:33:15 2022
+@author: souja
+"""
+
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    return "Welcome to my home 1"
+    
+@app.route("/how_are_you")
+def hello():
+    return "I am Good. What about u"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
